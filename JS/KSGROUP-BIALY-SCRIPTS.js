fc443c16bad248e6480160c3b8f7e0eca19534ad
Sebastian <kozlowskisebastian.pl@outlook.com>
2025-01-13 21:08:47 +0100

    Update KSGROUP-BIALY-SCRIPTS.js

--- JS/KSGROUP-BIALY-SCRIPTS.js
+++ JS/KSGROUP-BIALY-SCRIPTS.js
@@ -1,33 +1,48 @@
 const body = document.body;
 const svg = document.getElementById('KSGROUP-BIALY-SVG');
+const webp = document.getElementById('krakow');
 
 // Funkcja resetująca efekt
 function resetEffect() {
     body.classList.remove('hover-active');
-    svg.style.filter = 'brightness(0)';
+    if (svg) svg.style.filter = 'brightness(0)';
+    if (webp) webp.style.filter = 'brightness(0)';
 }
 
 // Funkcja aktywująca efekt
 function activateEffect() {
     body.classList.add('hover-active');
-    svg.style.filter = 'brightness(1) invert(1)';
+    if (svg) svg.style.filter = 'brightness(1) invert(1)';
+    if (webp) webp.style.filter = 'brightness(1) invert(1)';
 }
 
 // Reakcja na dotyk na urządzeniach mobilnych
-svg.addEventListener('touchstart', function() {
-    if (body.classList.contains('hover-active')) {
-        resetEffect();
-    } else {
-        activateEffect();
-    }
-});
+if (webp) {
+    webp.addEventListener('touchstart', function () {
+        if (body.classList.contains('hover-active')) {
+            resetEffect();
+        } else {
+            activateEffect();
+        }
+    });
+}
+
+if (svg) {
+    svg.addEventListener('touchstart', function () {
+        if (body.classList.contains('hover-active')) {
+            resetEffect();
+        } else {
+            activateEffect();
+        }
+    });
+}
 
 // Reakcja na najechanie myszką
-body.addEventListener('mouseover', function() {
+body.addEventListener('mouseover', function () {
     activateEffect();
 });
 
 // Reakcja na opuszczenie kursora
-body.addEventListener('mouseout', function() {
+body.addEventListener('mouseout', function () {
     resetEffect();
 });

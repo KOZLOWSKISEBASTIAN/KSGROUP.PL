6353148550c569e58d2bd0d1286dd0f0af8125d6
Sebastian <kozlowskisebastian.pl@outlook.com>
2025-01-13 21:28:03 +0100

    Update KSGROUP-BIALY-SCRIPTS.js

--- JS/KSGROUP-BIALY-SCRIPTS.js
+++ JS/KSGROUP-BIALY-SCRIPTS.js
@@ -1,32 +1,19 @@
 const body = document.body;
 const svg = document.getElementById('KSGROUP-BIALY-SVG');
-const webp = document.getElementById('krakow');
 
 // Funkcja resetująca efekt
 function resetEffect() {
     body.classList.remove('hover-active');
     if (svg) svg.style.filter = 'brightness(0)';
-    if (webp) webp.style.filter = 'brightness(0)';
 }
 
 // Funkcja aktywująca efekt
 function activateEffect() {
     body.classList.add('hover-active');
     if (svg) svg.style.filter = 'brightness(1) invert(1)';
-    if (webp) webp.style.filter = 'brightness(1) invert(1)';
 }
 
 // Reakcja na dotyk na urządzeniach mobilnych
-if (webp) {
-    webp.addEventListener('touchstart', function () {
-        if (body.classList.contains('hover-active')) {
-            resetEffect();
-        } else {
-            activateEffect();
-        }
-    });
-}
-
 if (svg) {
     svg.addEventListener('touchstart', function () {
         if (body.classList.contains('hover-active')) {

11a186f020a204e11b1e26ad71ac67fbc0afde12
Sebastian <kozlowskisebastian.pl@outlook.com>
2025-01-13 20:23:47 +0100

    Update KSGROUP-BIALY-SCRIPTS.js

--- JS/KSGROUP-BIALY-SCRIPTS.js
+++ JS/KSGROUP-BIALY-SCRIPTS.js
@@ -4,30 +4,36 @@ const svg = document.getElementById('KSGROUP-BIALY-WEKTOR-SVG');
 // Funkcja resetująca efekt
 function resetEffect() {
     body.classList.remove('hover-active');
-    svg.style.filter = 'brightness(0)';
+    if (svg) {
+        svg.style.filter = 'brightness(0)';
+    }
 }
 
 // Funkcja aktywująca efekt
 function activateEffect() {
     body.classList.add('hover-active');
-    svg.style.filter = 'brightness(1) invert(1)';
+    if (svg) {
+        svg.style.filter = 'brightness(1) invert(1)';
+    }
 }
 
 // Reakcja na dotyk na urządzeniach mobilnych
-svg.addEventListener('touchstart', function() {
-    if (body.classList.contains('hover-active')) {
-        resetEffect();
-    } else {
+if (svg) {
+    svg.addEventListener('touchstart', function () {
+        if (body.classList.contains('hover-active')) {
+            resetEffect();
+        } else {
+            activateEffect();
+        }
+    });
+
+    // Reakcja na najechanie myszką
+    body.addEventListener('mouseover', function () {
         activateEffect();
-    }
-});
+    });
 
-// Reakcja na najechanie myszką
-body.addEventListener('mouseover', function() {
-    activateEffect();
-});
-
-// Reakcja na opuszczenie kursora
-body.addEventListener('mouseout', function() {
-    resetEffect();
-});
+    // Reakcja na opuszczenie kursora
+    body.addEventListener('mouseout', function () {
+        resetEffect();
+    });
+}

92f982e2d08b08514a9c39a46fad46c07d3ac8e6
Sebastian <kozlowskisebastian.pl@outlook.com>
2025-01-13 23:04:40 +0100

    Create KSGROUP-BIALY-SCRIPTS.js

---
+++ JS/KSGROUP-BIALY-SCRIPTS.js
@@ -0,0 +1,44 @@
+const body = document.body;
+const imageContainer = document.getElementById('KSGROUP-LOGO-SVG-WEBP');
+// Funkcja do wykrywania urządzenia mobilnego
+function isMobileDevice() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return /android|ipad|iphone|ipod|blackberry|bb10|windows phone|mobile/i.test(userAgent);
+}
+// Funkcja ustawiająca grafikę
+function setImage() {
+    const isMobile = isMobileDevice();
+    const imageUrl = isMobile
+        ? "GRAFIKA/KSGROUP-BIALY-WEBP.webp" // Dla urządzeń mobilnych
+        : "GRAFIKA/KSGROUP-BIALY-SVG.svg"; // Dla komputerów
+
+    imageContainer.setAttribute("src", imageUrl);
+}
+// Funkcja resetująca efekt
+function resetEffect() {
+    body.classList.remove('hover-active');
+    imageContainer.style.filter = 'brightness(0)';
+}
+// Funkcja aktywująca efekt
+function activateEffect() {
+    body.classList.add('hover-active');
+    imageContainer.style.filter = 'brightness(1) invert(1)';
+}
+// Reakcja na dotyk na urządzeniach mobilnych
+imageContainer.addEventListener('touchstart', function () {
+    if (body.classList.contains('hover-active')) {
+        resetEffect();
+    } else {
+        activateEffect();
+    }
+});
+// Reakcja na najechanie myszką
+body.addEventListener('mouseover', function () {
+    activateEffect();
+});
+// Reakcja na opuszczenie kursora
+body.addEventListener('mouseout', function () {
+    resetEffect();
+});
+// Ustaw grafikę na podstawie urządzenia
+setImage();
